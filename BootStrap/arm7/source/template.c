1546b7d7a6828e5be870c766ff0319b9f386ee05
Dave Murphy <wntrmute@users.sourceforge.net>
2010-03-03 21:16:28 +0000

    update copyrights

--- BootStrap/arm7/source/template.c
+++ BootStrap/arm7/source/template.c
@@ -2,10 +2,10 @@
 
 default ARM7 core
 
-Copyright (C) 2005
-Michael Noland (joat)
-Jason Rogers (dovoto)
-Dave Murphy (WinterMute)
+Copyright (C) 2005 - 2010
+	Michael Noland (joat)
+	Jason Rogers (dovoto)
+	Dave Murphy (WinterMute)
 
 This software is provided 'as-is', without any express or implied
 warranty.  In no event will the authors be held liable for any
@@ -16,20 +16,22 @@ purpose, including commercial applications, and to alter it and
 redistribute it freely, subject to the following restrictions:
 
 1.	The origin of this software must not be misrepresented; you
-must not claim that you wrote the original software. If you use
-this software in a product, an acknowledgment in the product
-documentation would be appreciated but is not required.
+	must not claim that you wrote the original software. If you use
+	this software in a product, an acknowledgment in the product
+	documentation would be appreciated but is not required.
+
 2.	Altered source versions must be plainly marked as such, and
-must not be misrepresented as being the original software.
+	must not be misrepresented as being the original software.
+
 3.	This notice may not be removed or altered from any source
-distribution.
+	distribution.
 
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
 
 //---------------------------------------------------------------------------------
 void VcountHandler() {
-	//---------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
 	inputGetAndSend();
 }
 
@@ -37,13 +39,14 @@ void VcountHandler() {
 int main() {
 //---------------------------------------------------------------------------------
 	irqInit();
-	fifoInit();
 
 	// read User Settings from firmware
 	readUserSettings();
 
 	// Start the RTC tracking IRQ
 	initClockIRQ();
+
+	fifoInit();
 
 	SetYtrigger(80);
 
