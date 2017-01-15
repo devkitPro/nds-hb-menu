585a264eea652f95fa91a0d82300b0eb21433e82
Dave Murphy <davem@devkitpro.org>
2017-01-15 13:50:41 +0000

    build dsiware compatible bootstrap

--- BootStrap/arm9/source/bootstrap.c
+++ BootStrap/arm9/source/bootstrap.c
@@ -17,7 +17,6 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 ------------------------------------------------------------------*/
-const int __secure_area__ = 0;
 
 #include <nds.h>
 #include <fat.h>
