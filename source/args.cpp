aebbb0e9ff05ffc188dc6cb4319b8f4d00e09361
Michael "Chishm" Chisholm <chishm@chishm.com>
2019-01-28 15:18:58 +1100

    Add support for file extension handlers
    File extension handlers are files named "<type>.ext" where <type>
    specifies the file type to associate it with. The contents of *.ext
    files are argument lists (like .argv files) that point to an NDS file
    and provide optional arguments too. They are placed in the /nds
    directory.
    
    Handlers allow browsing for data files, which are then opened by the
    correct NDS file.
    
    For example, a file named "mp3.ext" containing "/nds/music-player.avi" will
    allow .mp3 files to be found and opened with music-player.avi. Another
    file named "m3u.ext" containing "/nds/music-player.avi --list" will let
    .m3u files be opened with music-player.avi, and the passed-in command
    line will be "/nds/music-player --list /path/to/file.m3u" (sans quotes).

--- source/args.cpp
+++ source/args.cpp
@@ -21,6 +21,7 @@
 
 ------------------------------------------------------------------*/
 
+#include <dirent.h>
 #include <limits.h>
 #include <stdio.h>
 #include <string>
@@ -35,6 +36,8 @@ using namespace std;
 
 static const string NDS_EXT = ".nds";
 static const string ARG_EXT = ".argv";
+static const string EXT_EXT = ".ext";
+static const char EXT_DIR[] = "/nds";
 static const char SEPARATORS[] = "\n\r\t ";
 
 /* Checks if s1 ends with s2, ignoring case.
@@ -161,14 +164,52 @@ static bool toAbsPath(const string& filename, const char* basePath, string& file
 	return true;
 }
 
+/* Convert a dataFilePath to the path of the ext file that specifies the
+ * handler.
+ * Returns true on success, false on failure
+ */
+static bool toExtPath(const string& dataFilePath, string& extFilePath) {
+	// Figure out what the file extension is
+	size_t extPos = dataFilePath.rfind('.');
+	if (extPos == string::npos) {
+		return false;
+	}
+
+	extPos += 1;
+	if (extPos >= dataFilePath.size()) {
+		return false;
+	}
+
+	// Construct handler path from extension. Handlers are in the EXT_DIR and
+	// end with EXT_EXT.
+	const string ext = dataFilePath.substr(extPos);
+	if (!toAbsPath(ext, EXT_DIR, extFilePath)) {
+		return false;
+	}
+
+	extFilePath += EXT_EXT;
+
+	return true;
+}
+
 bool argsNdsPath(const std::string& filePath, std::string& ndsPath) {
 	if (strCaseEnd(filePath, NDS_EXT)) {
 		ndsPath = filePath;
 		return true;
-	}
-
-	if (strCaseEnd(filePath, ARG_EXT)) {
+	} else	if (strCaseEnd(filePath, ARG_EXT)) {
 		return parseArgFileNds(filePath, ndsPath);
+	} else {
+		// This is a data file associated with a handler NDS by an ext file
+		string extPath;
+		if (!toExtPath(filePath, extPath)) {
+			return false;
+		}
+		string ndsRelPath;
+		if (!parseArgFileNds(extPath, ndsRelPath)) {
+			return false;
+		}
+		// Handler is in EXT_DIR
+		return toAbsPath(ndsRelPath, EXT_DIR, ndsPath);
 	}
 
 	return false;
@@ -184,12 +225,35 @@ bool argsFillArray(const string& filePath, vector<string>& argarray) {
 			return false;
 		}
 		argarray.push_back(move(absPath));
-	}
-
-	if (strCaseEnd(filePath, ARG_EXT)) {
+	} else if (strCaseEnd(filePath, ARG_EXT)) {
 		if (!parseArgFileAll(filePath, argarray)) {
 			return false;
 		}
+	} else {
+		// This is a data file associated with a handler NDS by an ext file
+		string extPath;
+
+		if (!toExtPath(filePath, extPath)) {
+			return false;
+		}
+
+		// Read the arg file for the extension handler
+		if (!parseArgFileAll(extPath, argarray)) {
+			return false;
+		}
+
+		// Extension handler relative path is relative to EXT_DIR, not CWD
+		string absPath;
+		if (!toAbsPath(argarray[0], EXT_DIR, absPath)) {
+			return false;
+		}
+		argarray[0] = absPath;
+
+		// Add the data filename to the end. Its path is relative to CWD.
+		if (!toAbsPath(filePath, NULL, absPath)) {
+			return false;
+		}
+		argarray.push_back(move(absPath));
 	}
 
 	return argarray.size() > 0 && strCaseEnd(argarray[0], NDS_EXT);
@@ -197,7 +261,28 @@ bool argsFillArray(const string& filePath, vector<string>& argarray) {
 
 vector<string> argsGetExtensionList() {
 	vector<string> extensionList;
+
+	// Always supported files: NDS binaries and predefined argument lists
 	extensionList.push_back(NDS_EXT);
 	extensionList.push_back(ARG_EXT);
+
+	// Get a list of extension files: argument lists associated with a file type
+	DIR *dir = opendir (EXT_DIR);
+	if (dir) {
+		for (struct dirent* dirent = readdir(dir); dirent != NULL; dirent = readdir(dir)) {
+			// Add the name component of all files ending with EXT_EXT to the list
+			if (dirent->d_type != DT_REG) {
+				continue;
+			}
+
+			if (dirent->d_name[0] != '.' && strCaseEnd(dirent->d_name, EXT_EXT)) {
+				size_t extPos = strlen(dirent->d_name) - EXT_EXT.size();
+				dirent->d_name[extPos] = '\0';
+				extensionList.push_back(dirent->d_name);
+			}
+		}
+		closedir(dir);
+	}
+
 	return extensionList;
 }
